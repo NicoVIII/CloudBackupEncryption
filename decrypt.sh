ac69ad4ae578ca6316970dd737f3e15f5d2cc72d
Nico Linder <nico@nicoviii.net>
2019-05-06 18:07:49 +0200

    try to implement output parameter (#16) * change internal way of implementing input parameter

--- decrypt.sh
+++ decrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR=$(dirname $0);DECRYPT=pgpbackup-decrypt;command -v pgpbackup-decrypt > /dev/null || DECRYPT="$DIR/$DECRYPT";
 
-exec $DECRYPT -u -- "$DIR"
+exec $DECRYPT -u -o "$DIR/../decrypted" -- "$DIR"
--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR=$(dirname $0);ENCRYPT=pgpbackup-encrypt;command -v pgpbackup-encrypt > /dev/null || ENCRYPT="$DIR/$ENCRYPT";
 
-exec $ENCRYPT -r <email_to_encrypt_for> -- "$DIR"
+exec $ENCRYPT -r <email_to_encrypt_for> -o "$DIR/../backup" -- "$DIR"
