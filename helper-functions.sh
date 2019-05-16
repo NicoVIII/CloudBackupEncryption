46a9577b6d72a47741bdf20c705bc0ae5113f191
Nico Linder <nico@nicoviii.net>
2019-05-16 11:12:42 +0200

    some more code improvements

--- helper-functions.sh
+++ helper-functions.sh
@@ -1,60 +1,22 @@
 #!/bin/bash
-# Printing functions
-function printHelper {
-    local message=$1
-    local verbose=$2
-    local error=$3
-
-    if [ "$_arg_quiet" == "off" ]; then
-        if [ $verbose == "off" ] || [ "$_arg_verbose" == "on" ]; then
-            if [ $error == "on" ]; then
-                echo >&2 "$message"
-            else
-                # Allow complete overwriting of progress bar
-                printf "%-34s\n" "$message"
-            fi
-        fi
-    fi
-}
-
-function printLog {
-    local message=$1
-
-    printHelper "$message" "off" "off"
-}
-
-function printVerbose {
-    local message=$1
-
-    printHelper "$message" "on" "off"
-}
-
-function printError {
-    local message=$1
-
-    printHelper "$message" "off" "on"
-}
-
 function dependencyMissing {
     local command=$1
 
-    ! command -v "$command" >/dev/null 2>&1 && printError "$command command not found!"
+    ! command -v "$command" >/dev/null 2>&1 && printErrorN "$command command not found!"
 }
 
 function execute {
     local command=$1
 
-    printVerbose "> $command"
+    printVerboseN "> $command"
     eval "$command"
 }
 
 function printProgress {
     if [ "$_arg_quiet" = "off" ]; then
-        encryptedBytes=$(cat "$encryptedBytesPipe")
+        local ratio=$[currSize * 100 / size]
 
-        local ratio=$[encryptedBytes * 100 / size]
-
-        local message="["
+        local message=" ["
 
         for i in {1..25}
         do
@@ -70,8 +32,7 @@ function printProgress {
         if [[ $ratio -ge 100 ]]; then
             echo "$message"
         else
-            echo -n "$message"
-            echo -en "\r"
+            echo -en "$message\r"
         fi
     fi
 }
@@ -80,9 +41,7 @@ function addToProgress {
     local addBytes=$1
 
     if [ "$_arg_quiet" = "off" ]; then
-        encryptedBytes=$(cat "$encryptedBytesPipe")
-        encryptedBytes=$[encryptedBytes + addBytes]
-        echo "$encryptedBytes" > $encryptedBytesPipe
+        currSize=$[currSize + addBytes]
         printProgress
     fi
 }
--- init-code.sh
+++ init-code.sh
@@ -1,6 +1,74 @@
 #!/bin/bash
 readonly inFolder="${_arg_input#"/"}"
 readonly outFolder="${_arg_output#"/"}"
+currSize=0
+
+# Printing functions
+function printHelper {
+    local message=$1
+    local verbose=$2
+    local error=$3
+    local ending=$4
+
+    if [ "$_arg_quiet" == "off" ]; then
+        if [ $verbose == "off" ] || [ "$_arg_verbose" == "on" ]; then
+            if [ $error == "on" ]; then
+                echo >&2 -en "$message$ending"
+            else
+                # Allow complete overwriting of progress bar
+                printf "%-35s$ending" "$message"
+            fi
+        fi
+    fi
+}
+
+function printLog {
+    local message=$1
+    local ending=$2
+
+    printHelper "$message" "off" "off" "$ending"
+}
+
+function printLogN {
+    local message=$1
+
+    printLog "$message" "\n"
+}
+
+function printLogR {
+    local message=$1
+
+    printLog "$message" "\r"
+}
+
+function printVerbose {
+    local message=$1
+    local ending=$2
+
+    printHelper "$message" "on" "off" "$ending"
+}
+
+function printVerboseN {
+    local message=$1
+
+    printVerbose "$message" "\n"
+}
+
+function printError {
+    local message=$1
+    local ending=$2
+
+    printHelper "$message" "off" "on" "$ending"
+}
+
+function printErrorN {
+    local message=$1
+
+    printError "$message" "\n"
+}
+
+# Start of program
+printLogN "$progname $version - $libname"
 
 # Prepare tmp folder
 readonly rootFolder=$(dirname $outFolder)
@@ -17,7 +85,3 @@ else
 fi
 readonly tmpFolder="$rootFolder/$tmp"
 mkdir "$tmpFolder"
-
-# Create named pipe for file size counter
-readonly encryptedBytesPipe="$tmpFolder/encryptedBytes"
-echo 0 > "$encryptedBytesPipe"
